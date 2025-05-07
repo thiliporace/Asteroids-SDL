d9f84c9adc6e168939d3840841a9aff7126203cd
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2025-05-07 11:10:47 -0300

    Initial Commit

---
+++ Asteroids NDP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Asteroids NDP
+//
+//  Created by Thiago Liporace on 07/05/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
